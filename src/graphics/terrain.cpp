f4a4be2025f20d00119c71e92e35b7d2a752a72f
Meindert van Duin <meindertvanduin99@gmail.com>
2022-09-07 00:17:53 +0200

    calculate the normals for heightmap terrain

--- src/graphics/terrain.cpp
+++ src/graphics/terrain.cpp
@@ -7,33 +7,78 @@ namespace graphics {
         this->sprite = std::make_unique<Sprite16>(heightmap_path);
         
         MeshData mesh_data;
+        
+        int touch_vertices[this->sprite->width][this->sprite->height];
+        for (int i = 0; i < this->sprite->height; i++) {
+            for (int j = 0; j < this->sprite->width; j++) {
+                touch_vertices[j][i] = 0;
+            }
+        }
+        Vertex vertices[this->sprite->width][this->sprite->height];
+    
+        for (auto y = 0; y < this->sprite->height; y++) {
+            for (auto x = 0; x < this->sprite->width; x++) {
+                Vertex v;
+                v.pos.x = x;
+                v.pos.y = ((float)this->sprite->get_pixel(x, y) /  65535.0f) * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT; 
+                v.pos.z = y;
+                v.normal = { 0, 0, 0 };
+                v.textcoords = { 0, 0 };
 
-        for (auto y = 0; y < this->sprite->height - 1; y++)
+                vertices[x][y] = v;
+            }
+        }
+
+        for (auto y = 0; y < this->sprite->height - 1; y++) {
             for (auto x = 0; x < this->sprite->width - 1; x++) {
-                Vertex v1;
-                v1.pos.x = x;
-                v1.pos.y = ((float)this->sprite->get_pixel(x, y) /  65535.0f) * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT; 
-                v1.pos.z = y;
-                v1.normal = { 0, 1, 0 };
+                auto &v1 = vertices[x][y];
+                auto &v2 = vertices[x + 1][y];
+                auto &v3 = vertices[x][y + 1];
+                auto &v4 = vertices[x + 1][y + 1];
 
-                Vertex v2;
-                v2.pos.x = x + 1;
-                v2.pos.y = ((float)this->sprite->get_pixel(x + 1, y) /  65535.0f) * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT; 
-                v2.pos.z = y;
-                v2.normal = { 0, 1, 0 };
+                auto line1 = v3.pos - v2.pos;
+                auto line2 = v3.pos - v1.pos;
+                auto normal = glm::cross(line1, line2);
 
-                Vertex v3;
-                v3.pos.x = x;
-                v3.pos.y = ((float)this->sprite->get_pixel(x, y + 1) /  65535.0f) * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT; 
-                v3.pos.z = y + 1;
-                v3.normal = { 0, 1, 0 };
+                v1.normal += normal;
+                v2.normal += normal;
+                v3.normal += normal;
 
-                Vertex v4;
-                v4.pos.x = x + 1;
-                v4.pos.y = ((float)this->sprite->get_pixel(x + 1, y + 1) /  65535.0f) * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT; 
-                v4.pos.z = y + 1;
-                v4.normal = { 0, 1, 0 };
-                
+                touch_vertices[x][y]++;
+                touch_vertices[x + 1][y]++;
+                touch_vertices[x][y + 1]++;
+
+                line1 = v3.pos - v4.pos;
+                line2 = v3.pos - v2.pos;
+                normal = glm::cross(line1, line2);
+
+                v2.normal += normal;
+                v3.normal += normal;
+                v4.normal += normal;
+
+                touch_vertices[x + 1][y]++;
+                touch_vertices[x][y + 1]++;
+                touch_vertices[x + 1][y + 1]++;
+            }
+        }
+
+        for (auto y = 0; y < this->sprite->height; y++) {
+            for (auto x = 0; x < this->sprite->width; x++) {
+                if (touch_vertices[x][y] >= 0) {
+                    auto &v1 = vertices[x][y];
+                    v1.normal /= touch_vertices[x][y];
+                    v1.normal = glm::normalize(v1.normal);
+                }
+            }
+        }
+
+        for (auto y = 0; y < this->sprite->height - 1; y++) {
+            for (auto x = 0; x < this->sprite->width - 1; x++) {
+                auto &v1 = vertices[x][y];
+                auto &v2 = vertices[x + 1][y];
+                auto &v3 = vertices[x][y + 1];
+                auto &v4 = vertices[x + 1][y + 1];
+
                 mesh_data.vertices.push_back(v3);
                 mesh_data.vertices.push_back(v2);
                 mesh_data.vertices.push_back(v1);
@@ -42,7 +87,8 @@ namespace graphics {
                 mesh_data.vertices.push_back(v4);
                 mesh_data.vertices.push_back(v2);
             }
-
+        }
+        
         this->mesh = std::make_unique<Mesh>(&mesh_data);
         this->transform.pos.y = -20;
     }
