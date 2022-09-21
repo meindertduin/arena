3ec580593a11e15455070abae912bf60b2cfc4af
Meindert van Duin <meindertvanduin99@gmail.com>
2022-09-21 20:36:35 +0200

    make logger a class with static functions

--- src/logging.h
+++ src/logging.h
@@ -2,41 +2,54 @@
 
 #include <string>
 #include <stdexcept>
+#include <iostream>
 
-inline constexpr std::string& replace_str_format(std::string &str, std::string arg) {
-    auto pos = str.find("%s");
-    str.replace(pos, 2, arg);
-    return str;
-}
+static std::string EscSeqResetColor = "\x1b[0m";
+static std::string EscSeqFgGreen = "\x1b[92m";
+static std::string EscSeqFgYellow = "\x1b[93m";
 
-template<typename ...Args>
-inline void log_error(std::string error, Args... args) {
-    throw std::runtime_error((replace_str_format(error, args), ...));
-}
+class Logger {
+public:
+    template<typename ...Args>
+    static inline void log_error(std::string error, Args... args) {
+        throw std::runtime_error((replace_str_format(error, args), ...));
+    }
 
-template<>
-inline void log_error(const std::string error) {
-    throw std::runtime_error(error);
-}
+    static inline void log_error(const std::string error) {
+        throw std::runtime_error(error);
+    }
 
-inline void debug(const std::string& msg) {
-    color_write_console(msg, EscSeqFgGreen);
-}
+    static inline void debug(const std::string& msg) {
+        color_write_console(msg, EscSeqFgGreen);
+    }
 
-template<typename ...Args>
-inline void debug(std::string msg, Args... args) {
-    color_write_console((replace_str_format(msg, args), ...), EscSeqFgGreen);
-}
+    template<typename ...Args>
+    static inline void debug(std::string msg, Args... args) {
+        color_write_console((replace_str_format(msg, args), ...), EscSeqFgGreen);
+    }
 
-inline void warning(const std::string &msg) {
-    color_write_console(msg, EscSeqFgYellow);
-}
+    static inline void warning(const std::string &msg) {
+        color_write_console(msg, EscSeqFgYellow);
+    }
 
-template<typename ...Args>
-inline void warning(std::string msg, Args... args) {
-    color_write_console((replace_str_format(msg, args), ...), EscSeqFgYellow);
-}
+    template<typename ...Args>
+    static inline void warning(std::string msg, Args... args) {
+        color_write_console((replace_str_format(msg, args), ...), EscSeqFgYellow);
+    }
+private:
+    static inline void color_write_console(std::string msg, const std::string &color_sequence) {
+        auto str = color_sequence + msg + EscSeqResetColor;
+        std::cout << str << std::endl;
+    }
+
+    static inline constexpr std::string& replace_str_format(std::string &str, std::string arg) {
+        auto pos = str.find("%s");
+        str.replace(pos, 2, arg);
+        return str;
+    }
+};
 
 
-#define THROW_ERROR(...) log_error(__VA_ARGS__)
+
+#define THROW_ERROR(...) Logger::log_error(__VA_ARGS__)
 
