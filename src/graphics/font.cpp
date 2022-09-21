aaac11ea49692c7d3551c18eb03d776918fcb9c8
Meindert van Duin <meindertvanduin99@gmail.com>
2022-09-21 22:20:04 +0200

    add a simple abstraction for a font

--- src/graphics/font.cpp
+++ src/graphics/font.cpp
@@ -1,8 +1,68 @@
 #include "font.h"
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
+#include "../logging.h"
+
+#include <glad/glad.h>
 
 namespace graphics {
+    constexpr int MaxCharacters = 128;
 
+    inline FT_Library library;
+
+    void font_init() {
+        if (FT_Init_FreeType(&library)) {
+            THROW_ERROR("font_init: Failed to initialize FreeType font.");
+        }
+    }
+
+    void font_quit() {
+        FT_Done_FreeType(library);
+    }
+
+    TrueTypeFont::TrueTypeFont(const std::string &path, int size) {
+        FT_New_Face(library, path.c_str(), 0, &face);
+        FT_Set_Pixel_Sizes(face, 0, size);
+
+        for(unsigned char c = 0; c < MaxCharacters; c++) {
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+                THROW_ERROR("TrueTypeFont::TrueTypeFont: could not load character %s", std::to_string(c));
+            }
+
+            uint32_t texture;
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(
+                    GL_TEXTURE_2D,
+                    0,
+                    GL_RED,
+                    face->glyph->bitmap.width,
+                    face->glyph->bitmap.rows,
+                    0,
+                    GL_RED,
+                    GL_UNSIGNED_BYTE,
+                    face->glyph->bitmap.buffer
+            );
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            Glyph character = {
+                    texture,
+                    glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                    glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                    static_cast<int>(face->glyph->advance.x)
+            };
+
+            glyphs.insert(std::pair<char, Glyph>(c, character));
+        }
+    }
+
+    TrueTypeFont::~TrueTypeFont() {
+        FT_Done_Face(face);
+    }
+
+    Glyph& TrueTypeFont::get_glyph(unsigned char c) const {
+        return glyphs[c];
+    }
 }
--- src/graphics/font.h
+++ src/graphics/font.h
@@ -5,12 +5,32 @@
 
 #include "texture.h"
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 namespace graphics {
+    void font_init();
+    void font_quit();
+
     struct Glyph {
-        GpuTexture texture;
+        // GpuTexture texture;
+        uint32_t texture;
         glm::ivec2 size;
         glm::ivec2 bearing;
         int advance;
     };
 
+    class TrueTypeFont {
+    public:
+        TrueTypeFont(const std::string &path, int size);
+        ~TrueTypeFont();
+
+        TrueTypeFont(const TrueTypeFont &other) = delete;
+        TrueTypeFont& operator=(const TrueTypeFont &other) = delete;
+
+        [[nodiscard]] Glyph& get_glyph(unsigned char c) const;
+    private:
+        FT_Face face = nullptr;
+        std::map<unsigned char, Glyph> glyphs;
+    };
 }
