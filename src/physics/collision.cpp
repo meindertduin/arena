71aa0b657885f9684b95db84fcd5069cfb5c5ca8
Meindert van Duin <meindertvanduin99@gmail.com>
2022-10-31 19:21:24 +0100

    Create a base mesh-collider

--- src/physics/collision.cpp
+++ src/physics/collision.cpp
@@ -27,4 +27,21 @@ namespace physics {
     CollisionPoints SphereCollider::test(const Transform &transform, const SphereCollider *collider, const Transform &sphere_transform) const {
         return find_sphere_sphere_collision(this, transform, collider, sphere_transform);
     }
+
+    CollisionPoints MeshCollider::test(const Transform &transform, const Collider *collider,
+                                       const Transform &collider_transform) const {
+        return { };
+    }
+
+    CollisionPoints MeshCollider::test(const Transform &transform, const SphereCollider *collider,
+                                       const Transform &sphere_transform) const {
+        return { };
+    }
+
+    glm::vec3 MeshCollider::find_furthest_points(const glm::vec3 &direction) const {
+        glm::vec3 max_point;
+
+        float max_distance = -FLT_MAX;
+        return max_point;
+    }
 }
--- src/physics/collision.h
+++ src/physics/collision.h
@@ -2,6 +2,7 @@
 
 #include <glm/vec3.hpp>
 #include "../entity/ec_transform.h"
+#include "../graphics/mesh.h"
 
 namespace physics {
     using Transform = entity::ECTransform;
@@ -28,6 +29,8 @@ namespace physics {
                 const Transform &transform,
                 const SphereCollider* collider,
                 const Transform &collider_transform) const = 0;
+
+        [[nodiscard]] virtual glm::vec3 find_furthest_points(const glm::vec3 &direction) const = 0;
     };
 
     class SphereCollider : public Collider {
@@ -44,5 +47,24 @@ namespace physics {
                 const Transform &transform,
                 const SphereCollider* collider,
                 const Transform &sphere_transform) const override;
+
+        [[nodiscard]] glm::vec3 find_furthest_points(const glm::vec3 &direction) const override { };
+    };
+
+    class MeshCollider : public Collider {
+    public:
+        CollisionPoints test(
+                const Transform &transform,
+                const Collider* collider,
+                const Transform &collider_transform) const override;
+
+        CollisionPoints test(
+                const Transform &transform,
+                const SphereCollider* collider,
+                const Transform &sphere_transform) const override;
+
+        [[nodiscard]] glm::vec3 find_furthest_points(const glm::vec3 &direction) const override;
+    private:
+        std::shared_ptr<graphics::Mesh> m_mesh;
     };
 }
