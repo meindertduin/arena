f8fc377f321d343eda447c8e7ad380476196e4cd
Meindert van Duin <meindertvanduin99@gmail.com>
2022-11-03 21:44:21 +0100

    Fix weak_ptr stored in the cache returning nullptr shared_ptrs

--- src/assets/cache.cpp
+++ src/assets/cache.cpp
@@ -6,17 +6,23 @@
 namespace assets {
     template<>
     std::shared_ptr<graphics::Mesh> Cache::load_asset<graphics::Mesh>(const std::string& filename) {
-        return load_obj(filename);
+        auto mesh = load_obj(filename);
+        meshes[filename] = std::weak_ptr(mesh);
+        return mesh;
     }
 
     template<>
     std::shared_ptr<graphics::Terrain> Cache::load_asset<graphics::Terrain>(const std::string& filename) {
-        return load_terrain(filename);
+        auto terrain = load_terrain(filename);
+        terrains[filename] = std::weak_ptr(terrain);
+        return terrain;
     }
 
     template<>
     std::shared_ptr<graphics::Texture> Cache::load_asset<graphics::Texture>(const std::string &filename) {
-        return load_texture(filename);
+        auto texture = load_texture(filename);
+        textures[filename] = std::weak_ptr(texture);
+        return texture;
     }
 
     template<>
