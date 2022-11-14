4e929a1d6688b99b0a80f8289976069c210b8ccd
Meindert van Duin <meindertvanduin99@gmail.com>
2022-11-14 22:12:40 +0100

    Setup base functions for the octree

--- src/physics/octree.cpp
+++ src/physics/octree.cpp
@@ -1,7 +1,89 @@
 #include "octree.h"
 
 namespace physics {
-    void create_octree() {
+    OctreeNode::OctreeNode(float half_size, const glm::vec3 &center_pos) {
+        auto min = glm::vec3{ center_pos.x - half_size, center_pos.y - half_size, center_pos.z - half_size };
+        auto max = glm::vec3{ center_pos.x + half_size, center_pos.y + half_size, center_pos.z + half_size };
 
+        m_aabb = math::AABB { min, max };
+    }
+
+    enum OctalPart {
+        LeftUpperFurthest = 0,
+        RightUpperFurthest = 1,
+        LeftLowerFurthest = 2,
+        RightLowerFurthest = 3,
+        LeftUpperClosest = 4,
+        RightUpperClosest = 5,
+        LeftLowerClosest = 6,
+        RightLowerClosest = 7,
+    };
+
+    glm::vec3 create_octal_center(OctalPart part, const glm::vec3 &center, float quarter_size) {
+        switch (part) {
+            case LeftUpperFurthest:
+                return { center.x - quarter_size, center.y + quarter_size, center.z + quarter_size };
+            case RightUpperFurthest:
+                return { center.x + quarter_size, center.y + quarter_size, center.z + quarter_size };
+            case LeftLowerFurthest:
+                return { center.x - quarter_size, center.y - quarter_size, center.z + quarter_size };
+            case RightLowerFurthest:
+                return { center.x + quarter_size, center.y - quarter_size, center.z + quarter_size };
+            case LeftUpperClosest:
+                return { center.x - quarter_size, center.y + quarter_size, center.z - quarter_size };
+            case RightUpperClosest:
+                return { center.x + quarter_size, center.y + quarter_size, center.z - quarter_size };
+            case LeftLowerClosest:
+                return { center.x - quarter_size, center.y - quarter_size, center.z - quarter_size };
+            case RightLowerClosest:
+                return { center.x + quarter_size, center.y - quarter_size, center.z - quarter_size };
+        }
+    }
+
+    Octree::Octree(float half_size, int max_layers) : m_max_layers{max_layers} {
+        auto center = glm::vec3 { 0 };
+        m_root = new OctreeNode { half_size, center };
+
+        auto quarter_size = half_size / 2.0f;
+        add_node_layers(m_root, center, quarter_size, 0);
+    }
+
+    void Octree::fill_with_objects(const std::vector<PhysicsObject*> &physics_objects) {
+        for (auto object : physics_objects) {
+            m_root->add_value(object);
+        }
+    }
+
+    std::vector<OctreeNode*> Octree::get_colliding_nodes(PhysicsObject *object) {
+        auto &aabb = object->rigid_body()->collider()->aabb();
+        std::vector<OctreeNode*> nodes;
+
+        if (!m_root->inside(aabb)) {
+            return nodes;
+        }
+
+        for (auto root_child : *m_root) {
+            root_child->fill_inside_nodes(aabb, nodes);
+        }
+
+        return nodes;
+    }
+
+    void Octree::reset() {
+        m_root->reset();
+    }
+
+    void Octree::add_node_layers(OctreeNode *node, const glm::vec3 &center, float half_size, int layer) const {
+        if (layer >= m_max_layers) {
+            return;
+        }
+
+        for (auto part = 0; part < 8; part++) {
+            auto new_center = create_octal_center((OctalPart)part, center, half_size);
+            auto new_node = new OctreeNode { half_size, new_center };
+            node->add_child(new_node);
+
+            add_node_layers(new_node, new_center, half_size / 2.0f, layer + 1);
+        }
     }
 }
--- src/physics/octree.h
+++ src/physics/octree.h
@@ -3,34 +3,84 @@
 #include <array>
 #include <vector>
 #include "../math/box.h"
+#include "physics_object.h"
+
+#include <algorithm>
 
 namespace physics {
-    template<typename T>
+
     class OctreeNode {
     public:
-        [[nodiscard]] bool inside(const math::AABB &other) const {
+        OctreeNode(float half_size, const glm::vec3 & center_pos);
+
+        [[nodiscard]] constexpr bool inside(const math::AABB &other) const {
             return m_aabb.inside(other);
         }
 
-        void add_child(OctreeNode<T> &&node) {
-            m_children[m_count] = std::move(node);
+        [[nodiscard]] bool inside_all(const math::AABB &other) const {
+            if (m_count > 0) {
+                return std::ranges::all_of(m_children.begin(), m_children.end(), [&other](auto child) {
+                    return child->inside(other);
+                });
+            }
+
+            return true;
         }
 
-        void add_value(T* value) {
-            m_values.push_back(value);
+        void fill_inside_nodes(const math::AABB &aabb, std::vector<OctreeNode*> nodes) {
+            if (inside_all(aabb)) {
+                nodes.push_back(this);
+                return;
+            }
+
+            for (auto child : m_children) {
+                child->fill_inside_nodes(aabb, nodes);
+            }
         }
+
+        void reset() {
+            m_values.clear();
+            for (auto child : m_children) {
+                child->reset();
+            }
+        }
+
+        void add_child(OctreeNode *node) {
+            m_children[m_count++] = node;
+        }
+
+        constexpr void add_value(PhysicsObject* value) {
+            m_values.push_back(value);
+            auto &aabb = value->rigid_body()->collider()->aabb();
+            for (auto child : m_children) {
+                if (child->inside(aabb)) {
+                    child->add_value(value);
+                }
+            }
+        }
+
+        constexpr auto begin() { return m_children.begin(); }
+        constexpr auto end() { return m_children.end(); }
+
+        constexpr ALWAYS_INLINE std::vector<PhysicsObject*> values() { return m_values; }
     private:
-        std::vector<T*> m_values;
+        std::vector<PhysicsObject*> m_values;
         math::AABB m_aabb;
-        std::array<OctreeNode<T>, 8> m_children;
+        std::array<OctreeNode*, 8> m_children;
         int m_count {0};
     };
 
-    template<typename T>
     class Octree {
     public:
+        Octree(float half_size, int max_layers);
 
+        std::vector<OctreeNode *> get_colliding_nodes(PhysicsObject *object);
+        void fill_with_objects(const std::vector<PhysicsObject*>& physics_objects);
+        void reset();
     private:
-        OctreeNode<T>* root;
+        OctreeNode* m_root;
+        int m_max_layers;
+
+        void add_node_layers(OctreeNode *node, const glm::vec3 &center, float half_size, int layer) const;
     };
 }
