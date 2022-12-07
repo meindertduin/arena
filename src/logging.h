9b83d234ab25767b39dc065da6ae3580f7f12cf3
meindert <meindertvanduin99@gmail.com>
2022-12-07 20:05:59 +0100

    Refactor the logging

--- src/logging.h
+++ src/logging.h
@@ -10,17 +10,18 @@ static std::string EscSeqFgYellow = "\x1b[93m";
 
 class Logger {
 public:
+
     template<typename ...Args>
     static inline void log_error(std::string error, Args... args) {
-        throw std::runtime_error((replace_str_format(error, args), ...));
+        log(LogStatus::Error, (replace_str_format(error, args), ...));
     }
 
     static inline void log_error(const std::string& error) {
-        throw std::runtime_error(error);
+        log(LogStatus::Error, error);
     }
 
-    static inline void debug(const std::string& msg) {
-        color_write_console(msg, EscSeqFgGreen);
+    static inline void debug(const std::string& message) {
+        log(LogStatus::Debug, message);
     }
 
     template<typename ...Args>
@@ -28,24 +29,45 @@ public:
         color_write_console((replace_str_format(msg, args), ...), EscSeqFgGreen);
     }
 
-    static inline void warning(const std::string &msg) {
-        color_write_console(msg, EscSeqFgYellow);
+    static inline void warning(const std::string &message) {
+        log(LogStatus::Warning, message);
     }
 
     template<typename ...Args>
-    static inline void warning(std::string msg, Args... args) {
-        color_write_console((replace_str_format(msg, args), ...), EscSeqFgYellow);
+    static inline void warning(std::string message, Args... args) {
+        log(LogStatus::Warning, (replace_str_format(message, args), ...));
     }
 private:
-    static inline void color_write_console(std::string msg, const std::string &color_sequence) {
-        auto str = color_sequence + msg + EscSeqResetColor;
-        std::cout << str << std::endl;
-    }
+    enum class LogStatus {
+        Info,
+        Warning,
+        Debug,
+        Error,
+    };
 
     static inline constexpr std::string& replace_str_format(std::string &str, std::string arg) {
         auto pos = str.find("%s");
         str.replace(pos, 2, arg);
         return str;
+    }
+
+    static inline void log(LogStatus status, std::string message) {
+        switch (status) {
+            case LogStatus::Info:
+                message = "[info] " + message;
+                break;
+            case LogStatus::Warning:
+                message = EscSeqFgYellow + "[warning] " + message + EscSeqResetColor;
+                break;
+            case LogStatus::Error:
+                message = EscSeqFgYellow + "[error] " + message + EscSeqResetColor;
+                break;
+            case LogStatus::Debug:
+                message = EscSeqFgGreen + "[debug] " + message + EscSeqResetColor;
+                break;
+        }
+
+        std::cout << message << std::endl;
     }
 };
 
